594fde61ab769c25499d71c0025694b61a24f221
angelique-m08 <mora.angelique@gmail.com>
2020-03-31 15:26:58 +0200

    Created props

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,19 +1,25 @@
 import React from 'react';
+import './chat.css';
+import PropTypes from 'prop-types';
 
-const name = 'Kristin Curtis';
-const avatar = 'https://randomuser.me/api/portraits/women/63.jpg';
-const online = true;
-
-function Contact() {
+const Contact = (props) => {
     return (
         <div className="Contact">
-            <img className="avatar" src={avatar} alt="avatar" />
+            <img className="avatar" src={props.avatar} alt="avatar" />
             <div>
-               <p className="name">{name}</p>  
-               <p className="status"> <span className="status-online"></span> <span className="status-text"> {online ? 'Online' : 'Offline'} </span></p>
+               <h2 className="name">{props.name}</h2>  
+               <div className="status"> 
+                  <div className={props.status === "online" ? 'status-online' : 'status-offline'}></div>
+                  <p className="status-text">{props.status}</p>
+               </div>
             </div>
         </div>
     )
 }
+
+Contact.propTypes = {
+    name: PropTypes.string.isRequired,
+    status: PropTypes.string.isRequired,
+  };
 
 export default Contact;
