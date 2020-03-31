45aa66b74530f07ae5b0a505dbac14f303f691b6
angelique-m08 <mora.angelique@gmail.com>
2020-03-31 15:25:39 +0200

    Created props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Contact from './components/Contact'
-import './components/chat.css';
 
 function App() {
   return (
     <div>
-      <Contact/>
+      <Contact name="Kristin Curtis" avatar="https://randomuser.me/api/portraits/women/63.jpg" status="online" />
+      <Contact name="Alice Hunter" avatar="https://randomuser.me/api/portraits/women/35.jpg" status="offline" />
+      <Contact name="Kirk Burns" avatar="https://randomuser.me/api/portraits/men/79.jpg" status="online" />
     </div>
   );
 }
