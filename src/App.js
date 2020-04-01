f1c1a5db951597f640b65431d389b6dd34dda7f7
angelique-m08 <mora.angelique@gmail.com>
2020-04-01 11:25:31 +0200

    add module contactlist

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Contact from './components/Contact'
+import ContactList from './components/ContactList'
 
 function App() {
   return (
     <div>
-      <Contact name="Kristin Curtis" avatar="https://randomuser.me/api/portraits/women/63.jpg" status="online" />
-      <Contact name="Alice Hunter" avatar="https://randomuser.me/api/portraits/women/35.jpg" status="offline" />
-      <Contact name="Kirk Burns" avatar="https://randomuser.me/api/portraits/men/79.jpg" status="online" />
+      <Contact name="Kristin Curtis" avatar="https://randomuser.me/api/portraits/women/63.jpg" online />
+      <Contact name="Alice Hunter" avatar="https://randomuser.me/api/portraits/women/35.jpg" offline />
+      <Contact name="Kirk Burns" avatar="https://randomuser.me/api/portraits/men/79.jpg" online />
+      <ContactList />
     </div>
   );
 }
