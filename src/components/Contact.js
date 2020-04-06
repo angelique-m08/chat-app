95194b078c96ff2aecd4b1ed6d29cf7b0d35f53e
angelique-m08 <mora.angelique@gmail.com>
2020-04-06 11:17:04 +0200

    feat: add contact state for users status online or offline when clicked

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,20 +2,30 @@ import React from 'react';
 import './chat.css';
 import PropTypes from 'prop-types';
 
-const Contact = (props) => {
+class Contact extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          online: props.online
+        };
+    }
+    render() {
     return (
         <div className="Contact">
-            <img className="avatar" src={props.avatar} alt="avatar" />
+            <img className="avatar" src={this.props.avatar} alt="avatar" />
             <div>
-               <h2 className="name">{props.name}</h2>  
-               <div className="status"> 
-                  <div className={props.online ? 'status-online' : 'status-offline'}></div>
-                  <p className="status-text">{props.online ? 'online' : 'offline'}</p>
+               <h2 className="name">{this.props.name}</h2>  
+               <div className="status" onClick={event => {
+                    const newState = !this.state.online;
+                    this.setState({online: newState});
+                  }}> 
+                  <div className={this.state.online ? 'status-online' : 'status-offline'}></div>
+                  <p className="status-text">{this.state.online ? 'online' : 'offline'}</p>
                </div>
             </div>
         </div>
     )
-}
+}}
 
 Contact.propTypes = {
     name: PropTypes.string.isRequired,
